02d517316065c607b8d18e027a3961442478a771
Xepishons <Crysis10ivan@rambler.ru>
2017-04-06 02:30:47 +0300

    Ориентированный граф №2.

--- Tests/Tests.java
+++ Tests/Tests.java
@@ -1,117 +1,465 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import javafx.util.Pair;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tests {
-    public int size = 10;
-    public String[] name2 = new String[size];
-    public String[] name1 = new String[size];
-
-    {
-        name2[0] = "a";
-        name2[1] = "b";
-        name2[2] = "c";
-        name2[3] = "d";
-        name2[4] = "e";
-
-        name1[0] = "a";
-        name1[1] = "b";
-        name1[2] = "c";
-        name1[3] = "d";
-        name1[4] = "e";
-    }
-
-    public int[][] matrix2 = new int[size][size];
-    public int[][] matrix1 = new int[size][size];
-
-    {
-        for (int j = 0; j < size; j++)
-            for (int i = 0; i < size; i++)
-                matrix2[i][j] = 0;
-        matrix2[2][1] = 1;
-        matrix2[3][2] = 2;
-        matrix2[4][3] = 3;
-        matrix2[4][2] = 4;
-        matrix2[3][4] = 5;
-        matrix2[3][5] = 6;
-        matrix2[1][3] = 7;
-        matrix2[5][1] = 8;
-        matrix2[2][5] = 9;
-
-        matrix1[2][1] = 1;
-        matrix1[3][2] = 2;
-        matrix1[4][3] = 3;
-        matrix1[4][2] = 4;
-        matrix1[3][4] = 5;
-        matrix1[3][5] = 6;
-        matrix1[1][3] = 7;
-        matrix1[5][1] = 8;
-        matrix1[2][5] = 9;
-    }
 
     @Test
-    @Tag("addTrack")
-    public void addTrack() {
-        Graph asd = new Graph(matrix1, name1);
-        matrix2[1][2] = 10;
-        //System.out.print(asd.addTrack(10, "a", "b"));
-        assertEquals(asd.addTrack(10, "a", "b"), new Graph(matrix2, name2));
-    }
-
-    @Test
-    @Tag("deleteTrack")
-    public void deleteTrack() {
-        Graph asd = new Graph(matrix1, name1);
-        matrix2[3][5] = 0;
-        //System.out.print(asd.deleteTrack("c", "e"));
-        assertEquals(asd.deleteTrack("c", "e"), new Graph(matrix2, name2));
-    }
-
-    @Test
-    @Tag("renameTrack")
-    public void renameTrack() {
-        Graph asd = new Graph(matrix1, name1);
-        matrix2[3][5] = 1;
-        //System.out.print(asd.renameTrack(1, "c", "e"));
-        assertEquals(asd.renameTrack(1, "c", "e"), new Graph(matrix2, name2));
+    @Tag("deletePoint")
+    void deletePoint() {
+        ArrayList<List<Integer>> matr = new ArrayList<>();
+        List<Integer> matr1 = new ArrayList<>();
+        matr1.add(0);
+        matr1.add(4);
+        matr1.add(2);
+        matr1.add(-1);
+        matr.add(matr1);
+        List<Integer> matr2 = new ArrayList<>();
+        matr2.add(-1);
+        matr2.add(0);
+        matr2.add(7);
+        matr2.add(6);
+        matr.add(matr2);
+        List<Integer> matr3 = new ArrayList<>();
+        matr3.add(1);
+        matr3.add(9);
+        matr3.add(0);
+        matr3.add(3);
+        matr.add(matr3);
+        ArrayList<Integer> matr4 = new ArrayList<>();
+        matr4.add(-1);
+        matr4.add(5);
+        matr4.add(8);
+        matr4.add(0);
+        matr.add(matr4);
+        ArrayList<String> nnnn = new ArrayList<>();
+        nnnn.add("a");
+        nnnn.add("b");
+        nnnn.add("c");
+        nnnn.add("d");
+        Graph graphTest = new Graph(matr, nnnn);
+        Graph graph = new Graph(matr, nnnn);
+        List<List<Integer>> matr0 = new ArrayList<>();
+        ArrayList<Integer> matr12 = new ArrayList<>();
+        matr12.add(0);
+        matr12.add(4);
+        matr12.add(-1);
+        matr0.add(matr12);
+        ArrayList<Integer> matr22 = new ArrayList<>();
+        matr22.add(-1);
+        matr22.add(0);
+        matr22.add(6);
+        matr0.add(matr22);
+        ArrayList<Integer> matr32 = new ArrayList<>();
+        matr32.add(-1);
+        matr32.add(5);
+        matr32.add(0);
+        matr0.add(matr32);
+        ArrayList<String> nnnn2 = new ArrayList<>();
+        nnnn2.add("a");
+        nnnn2.add("b");
+        nnnn2.add("d");
+        graph = new Graph(matr0, nnnn2);
+        assertEquals(graphTest.deletePoint("c"), graph);
     }
 
     @Test
     @Tag("addPoint")
-    public void addPoint() {
-        Graph asd = new Graph(matrix1, name1);
-        name2[5] = "V";
-        //System.out.print(asd.addPoint("V"));
-        assertEquals(asd.addPoint("V"), new Graph(matrix2, name2));
+    void addPoint() {
+        List<List<Integer>> matr = new ArrayList<>();
+        ArrayList<Integer> matr1 = new ArrayList<>();
+        matr1.add(0);
+        matr1.add(2);
+        matr1.add(1);
+        matr1.add(null);
+        matr.add(matr1);
+        ArrayList<Integer> matr2 = new ArrayList<>();
+        matr2.add(8);
+        matr2.add(0);
+        matr2.add(1);
+        matr2.add(null);
+        matr.add(matr2);
+        ArrayList<Integer> matr3 = new ArrayList<>();
+        matr3.add(3);
+        matr3.add(2);
+        matr3.add(0);
+        matr3.add(null);
+        matr.add(matr3);
+        ArrayList<Integer> matr4 = new ArrayList<>();
+        matr4.add(null);
+        matr4.add(null);
+        matr4.add(null);
+        matr4.add(null);
+        matr.add(matr4);
+        ArrayList<String> nnnn = new ArrayList<>();
+        nnnn.add("a");
+        nnnn.add("b");
+        nnnn.add("c");
+        nnnn.add("d");
+        Graph graphTest = new Graph(matr, nnnn);
+        Graph graph = new Graph(matr, nnnn);
+        List<List<Integer>> matr0 = new ArrayList<>();
+        ArrayList<Integer> matr12 = new ArrayList<>();
+        matr12.add(0);
+        matr12.add(2);
+        matr12.add(1);
+        matr0.add(matr12);
+        ArrayList<Integer> matr22 = new ArrayList<>();
+        matr22.add(8);
+        matr22.add(0);
+        matr22.add(1);
+        matr0.add(matr22);
+        ArrayList<Integer> matr32 = new ArrayList<>();
+        matr32.add(3);
+        matr32.add(2);
+        matr32.add(0);
+        matr0.add(matr32);
+        ArrayList<String> nnnn2 = new ArrayList<>();
+        nnnn2.add("a");
+        nnnn2.add("b");
+        nnnn2.add("c");
+
+        graph = new Graph(matr0, nnnn2);
+        assertEquals(graph.addPoint("d"), graphTest);
     }
 
     @Test
-    @Tag("deletePoint")
-    public void deletePoint() {
-        Graph asd = new Graph(matrix1, name1);
-        name2[4] = null;
-        matrix2[0][5] = 0;
-        matrix2[1][5] = 0;
-        matrix2[2][5] = 0;
-        matrix2[3][5] = 0;
-        matrix2[4][5] = 0;
+    @Tag("addTrack")
+    void addTrack() {
+        List<List<Integer>> matr = new ArrayList<>();
+        ArrayList<Integer> matr1 = new ArrayList<>();
+        matr1.add(0);
+        matr1.add(4);
+        matr1.add(2);
+        matr1.add(2);
+        matr.add(matr1);
+        ArrayList<Integer> matr2 = new ArrayList<>();
+        matr2.add(-1);
+        matr2.add(0);
+        matr2.add(7);
+        matr2.add(6);
+        matr.add(matr2);
+        ArrayList<Integer> matr3 = new ArrayList<>();
+        matr3.add(1);
+        matr3.add(9);
+        matr3.add(0);
+        matr3.add(3);
+        matr.add(matr3);
+        ArrayList<Integer> matr4 = new ArrayList<>();
+        matr4.add(-1);
+        matr4.add(5);
+        matr4.add(8);
+        matr4.add(0);
+        matr.add(matr4);
+        ArrayList<String> nnnn = new ArrayList<>();
+        nnnn.add("a");
+        nnnn.add("b");
+        nnnn.add("c");
+        nnnn.add("d");
+        Graph graphTest = new Graph(matr, nnnn);
+        Graph graph = new Graph(matr, nnnn);
+        List<List<Integer>> matr0 = new ArrayList<>();
+        ArrayList<Integer> matr12 = new ArrayList<>();
+        matr12.add(0);
+        matr12.add(4);
+        matr12.add(2);
+        matr12.add(-1);
+        matr0.add(matr12);
+        ArrayList<Integer> matr22 = new ArrayList<>();
+        matr22.add(-1);
+        matr22.add(0);
+        matr22.add(7);
+        matr22.add(6);
+        matr0.add(matr22);
+        ArrayList<Integer> matr32 = new ArrayList<>();
+        matr32.add(1);
+        matr32.add(9);
+        matr32.add(0);
+        matr32.add(3);
+        matr0.add(matr32);
+        ArrayList<Integer> matr42 = new ArrayList<>();
+        matr42.add(-1);
+        matr42.add(5);
+        matr42.add(8);
+        matr42.add(0);
+        matr0.add(matr42);
+        ArrayList<String> nnnn2 = new ArrayList<>();
+        nnnn2.add("a");
+        nnnn2.add("b");
+        nnnn2.add("c");
+        nnnn2.add("d");
+        graph = new Graph(matr0, nnnn2);
+        assertEquals(graphTest, graph.addTrack("d", "a", 2));
+    }
 
-        matrix2[5][0] = 0;
-        matrix2[5][1] = 0;
-        matrix2[5][2] = 0;
-        matrix2[5][3] = 0;
-        matrix2[5][4] = 0;
-        //System.out.print(asd.deletePoint("e"));
-        assertEquals(asd.deletePoint("e"), new Graph(matrix2, name2));
+    @Test
+    @Tag("renameTrack")
+    void renameTrack() {
+        List<List<Integer>> matr = new ArrayList<>();
+        ArrayList<Integer> matr1 = new ArrayList<>();
+        matr1.add(0);
+        matr1.add(4);
+        matr1.add(2);
+        matr1.add(-1);
+        matr.add(matr1);
+        ArrayList<Integer> matr2 = new ArrayList<>();
+        matr2.add(-1);
+        matr2.add(0);
+        matr2.add(7);
+        matr2.add(6);
+        matr.add(matr2);
+        ArrayList<Integer> matr3 = new ArrayList<>();
+        matr3.add(1);
+        matr3.add(9);
+        matr3.add(0);
+        matr3.add(3);
+        matr.add(matr3);
+        ArrayList<Integer> matr4 = new ArrayList<>();
+        matr4.add(-1);
+        matr4.add(5);
+        matr4.add(10);
+        matr4.add(0);
+        matr.add(matr4);
+        ArrayList<String> nnnn = new ArrayList<>();
+        nnnn.add("a");
+        nnnn.add("b");
+        nnnn.add("c");
+        nnnn.add("d");
+        Graph graphTest = new Graph(matr, nnnn);
+        Graph graph = new Graph(matr, nnnn);
+        List<List<Integer>> matr0 = new ArrayList<>();
+        ArrayList<Integer> matr12 = new ArrayList<>();
+        matr12.add(0);
+        matr12.add(4);
+        matr12.add(2);
+        matr12.add(-1);
+        matr0.add(matr12);
+        ArrayList<Integer> matr22 = new ArrayList<>();
+        matr22.add(-1);
+        matr22.add(0);
+        matr22.add(7);
+        matr22.add(6);
+        matr0.add(matr22);
+        ArrayList<Integer> matr32 = new ArrayList<>();
+        matr32.add(1);
+        matr32.add(9);
+        matr32.add(0);
+        matr32.add(3);
+        matr0.add(matr32);
+        ArrayList<Integer> matr42 = new ArrayList<>();
+        matr42.add(-1);
+        matr42.add(5);
+        matr42.add(8);
+        matr42.add(0);
+        matr0.add(matr42);
+        ArrayList<String> nnnn2 = new ArrayList<>();
+        nnnn2.add("a");
+        nnnn2.add("b");
+        nnnn2.add("c");
+        nnnn2.add("d");
+        graph = new Graph(matr0, nnnn2);
+        assertEquals(graphTest, graph.renameTrack("c", "d", 10));
+    }
+
+    @Test
+    @Tag("renameTrack")
+    void deleteTrack() {
+        List<List<Integer>> matr = new ArrayList<>();
+        ArrayList<Integer> matr1 = new ArrayList<>();
+        matr1.add(0);
+        matr1.add(null);
+        matr1.add(2);
+        matr1.add(-1);
+        matr.add(matr1);
+        ArrayList<Integer> matr2 = new ArrayList<>();
+        matr2.add(-1);
+        matr2.add(0);
+        matr2.add(7);
+        matr2.add(6);
+        matr.add(matr2);
+        ArrayList<Integer> matr3 = new ArrayList<>();
+        matr3.add(1);
+        matr3.add(9);
+        matr3.add(0);
+        matr3.add(3);
+        matr.add(matr3);
+        ArrayList<Integer> matr4 = new ArrayList<>();
+        matr4.add(-1);
+        matr4.add(5);
+        matr4.add(8);
+        matr4.add(0);
+        matr.add(matr4);
+        ArrayList<String> nnnn = new ArrayList<>();
+        nnnn.add("a");
+        nnnn.add("b");
+        nnnn.add("c");
+        nnnn.add("d");
+        Graph graphTest = new Graph(matr, nnnn);
+        Graph graph = new Graph(matr, nnnn);
+        List<List<Integer>> matr0 = new ArrayList<>();
+        ArrayList<Integer> matr12 = new ArrayList<>();
+        matr12.add(0);
+        matr12.add(4);
+        matr12.add(2);
+        matr12.add(-1);
+        matr0.add(matr12);
+        ArrayList<Integer> matr22 = new ArrayList<>();
+        matr22.add(-1);
+        matr22.add(0);
+        matr22.add(7);
+        matr22.add(6);
+        matr0.add(matr22);
+        ArrayList<Integer> matr32 = new ArrayList<>();
+        matr32.add(1);
+        matr32.add(9);
+        matr32.add(0);
+        matr32.add(3);
+        matr0.add(matr32);
+        ArrayList<Integer> matr42 = new ArrayList<>();
+        matr42.add(-1);
+        matr42.add(5);
+        matr42.add(8);
+        matr42.add(0);
+        matr0.add(matr42);
+        ArrayList<String> nnnn2 = new ArrayList<>();
+        nnnn2.add("a");
+        nnnn2.add("b");
+        nnnn2.add("c");
+        nnnn2.add("d");
+        graph = new Graph(matr0, nnnn2);
+        assertEquals(graphTest, graph.deleteTrack("b", "a"));
+    }
+
+    @Test
+    @Tag("input")
+    void input() {
+        List<List<Integer>> matr = new ArrayList<>();
+        ArrayList<Integer> matr1 = new ArrayList<>();
+        matr1.add(0);
+        matr1.add(4);
+        matr1.add(2);
+        matr1.add(-1);
+        matr.add(matr1);
+        ArrayList<Integer> matr2 = new ArrayList<>();
+        matr2.add(-1);
+        matr2.add(0);
+        matr2.add(7);
+        matr2.add(6);
+        matr.add(matr2);
+        ArrayList<Integer> matr3 = new ArrayList<>();
+        matr3.add(1);
+        matr3.add(9);
+        matr3.add(0);
+        matr3.add(3);
+        matr.add(matr3);
+        ArrayList<Integer> matr4 = new ArrayList<>();
+        matr4.add(-1);
+        matr4.add(5);
+        matr4.add(8);
+        matr4.add(0);
+        matr.add(matr4);
+        ArrayList<String> nnnn = new ArrayList<>();
+        nnnn.add("a");
+        nnnn.add("b");
+        nnnn.add("c");
+        nnnn.add("d");
+        ArrayList<Pair<String, Integer>> test = new ArrayList<>();
+        test.add(new Pair("a", -1));
+        test.add(new Pair("b", 0));
+        test.add(new Pair("c", 7));
+        test.add(new Pair("d", 6));
+        Graph graphTest = new Graph(matr, nnnn);
+        assertEquals(graphTest.input("b"), test);
+    }
+
+    @Test
+    @Tag("output")
+    void output() {
+        List<List<Integer>> matr = new ArrayList<>();
+        ArrayList<Integer> matr1 = new ArrayList<>();
+        ArrayList<Pair<String, Integer>> test = new ArrayList<>();
+        matr1.add(0);
+        matr1.add(4);
+        matr1.add(2);
+        test.add(new Pair("a", 2));
+        matr1.add(-1);
+        matr.add(matr1);
+        ArrayList<Integer> matr2 = new ArrayList<>();
+        matr2.add(-1);
+        matr2.add(0);
+        matr2.add(7);
+        test.add(new Pair("b", 7));
+        matr2.add(6);
+        matr.add(matr2);
+        ArrayList<Integer> matr3 = new ArrayList<>();
+        matr3.add(1);
+        matr3.add(9);
+        matr3.add(0);
+        test.add(new Pair("c", 0));
+        matr3.add(3);
+        matr.add(matr3);
+        ArrayList<Integer> matr4 = new ArrayList<>();
+        matr4.add(-1);
+        matr4.add(5);
+        matr4.add(8);
+        test.add(new Pair("d", 8));
+        matr4.add(0);
+        matr.add(matr4);
+        ArrayList<String> nnnn = new ArrayList<>();
+        nnnn.add("a");
+        nnnn.add("b");
+        nnnn.add("c");
+        nnnn.add("d");
+        Graph graphTest = new Graph(matr, nnnn);
+        assertEquals(graphTest.output("c"), test);
     }
 
     @Test
     @Tag("renamePoint")
-    public void renamePoint() {
-        Graph asd = new Graph(matrix1, name1);
-        name2[4] = "V";
-        //System.out.print(asd.renamePoint("e", "V"));
-        assertEquals(asd.renamePoint("e", "V"), new Graph(matrix2, name2));
+    void renamePoint() {
+        List<List<Integer>> matr = new ArrayList<>();
+        ArrayList<Integer> matr1 = new ArrayList<>();
+        ArrayList<Integer> test = new ArrayList<>();
+        matr1.add(0);
+        matr1.add(4);
+        matr1.add(2);
+        matr1.add(10);
+        matr.add(matr1);
+        ArrayList<Integer> matr2 = new ArrayList<>();
+        matr2.add(-1);
+        matr2.add(0);
+        matr2.add(-7);
+        matr2.add(6);
+        matr.add(matr2);
+        ArrayList<Integer> matr3 = new ArrayList<>();
+        matr3.add(1);
+        matr3.add(9);
+        matr3.add(0);
+        matr3.add(3);
+        matr.add(matr3);
+        ArrayList<Integer> matr4 = new ArrayList<>();
+        matr4.add(-1);
+        matr4.add(5);
+        matr4.add(8);
+        matr4.add(0);
+        matr.add(matr4);
+        ArrayList<String> nnnn = new ArrayList<>();
+        nnnn.add("a");
+        nnnn.add("b");
+        nnnn.add("c");
+        nnnn.add("d");
+        ArrayList<String> nnnnT = new ArrayList<>();
+        nnnnT.add("a");
+        nnnnT.add("b");
+        nnnnT.add("f");
+        nnnnT.add("d");
+        Graph graphTest = new Graph(matr, nnnn);
+        Graph graph = new Graph(matr, nnnnT);
+        assertEquals(graphTest.renamePoint("c", "f"), graph);
     }
+
 }
--- src/Graph.java
+++ src/Graph.java
@@ -1,76 +1,35 @@
-import java.util.Arrays;
+import javafx.util.Pair;
+
+import java.util.*;
 
 public class Graph {
-    private int size = 10;
-    private String[] name = new String[]{};
-    private int[][] matrix = new int[size][size];
+    private List<String> name;
+    private List<List<Integer>> matrix;
 
-    public Graph(int[][] matrix, String[] name) {
+    public Graph(List<List<Integer>> a, List<String> b) {
+        List<List<Integer>> matrix = new ArrayList<>();
+        for (int i = 0; i < a.size(); i++) {
+            matrix.add(new ArrayList<>());
+            for (int j = 0; j < a.get(i).size(); j++) {
+                List<Integer> list = matrix.get(i);
+                list.add(a.get(i).get(j));
+                matrix.set(i, list);
+            }
+        }
+        List<String> name = new ArrayList<>();
+        for (int i = 0; i < b.size(); i++) {
+            name.add(b.get(i));
+        }
         this.matrix = matrix;
         this.name = name;
-    }
-
-    public Graph addTrack(Integer length, String begin, String end) {
-        int x = 0, y = 0;
-        for (int i = 0; i < size; i++) {
-            if (name[i] == begin) x = i + 1;
-            if (name[i] == end) y = i + 1;
+        int i, size = matrix.size();
+        for (i = 0; i < matrix.size(); i++) {
+            if (matrix.get(i).size() > size) size = matrix.get(i).size();
         }
-        this.matrix[x][y] = length;
-        return new Graph(this.matrix, this.name);
-    }
-
-    public Graph deleteTrack(String begin, String end) {
-        int x = 0, y = 0;
-        for (int i = 0; i < size; i++) {
-            if (name[i] == begin) x = i + 1;
-            if (name[i] == end) y = i + 1;
-        }
-        matrix[x][y] = 0;
-        return new Graph(this.matrix, this.name);
-    }
-
-    public Graph renameTrack(Integer length, String begin, String end) {
-        int x = 0, y = 0;
-        for (int i = 0; i < size; i++) {
-            if (name[i] == begin) x = i + 1;
-            if (name[i] == end) y = i + 1;
-        }
-        matrix[x][y] = length;
-        return new Graph(this.matrix, this.name);
-    }
-
-    public Graph addPoint(String point) {
-        int x = 0;
-        for (int i = 0; i < size; i++)
-            if (name[i] == null) {
-                x = i;
-                break;
+        for (i = 0; i < name.size(); i++)
+            if (this.matrix.size() <= i) {
+                this.matrix.add(new ArrayList<>());
             }
-        name[x] = point;
-        return new Graph(this.matrix, this.name);
-    }
-
-    public Graph deletePoint(String point) {
-        int x = 0;
-        for (int i = 0; i < size; i++) {
-            if (name[i] == point) {
-                name[i] = null;
-                x = i + 1;
-            }
-        }
-        for (int i = 0; i < x; i++) {
-            matrix[i][x] = 0;
-            matrix[x][i] = 0;
-        }
-        return new Graph(this.matrix, this.name);
-    }
-
-    public Graph renamePoint(String name1, String name2) {
-        for (int i = 0; i < size; i++) {
-            if (name[i] == name1) name[i] = name2;
-        }
-        return new Graph(this.matrix, this.name);
     }
 
     @Override
@@ -80,34 +39,71 @@ public class Graph {
 
         Graph graph = (Graph) o;
 
-        if (size != graph.size) return false;
-        if (!Arrays.equals(name, graph.name)) return false;
-        return Arrays.deepEquals(matrix, graph.matrix);
+        if (!getMatrix().equals(graph.getMatrix())) return false;
+        return getName().equals(graph.getName());
     }
 
     @Override
     public int hashCode() {
-        int result = size;
-        result = 31 * result + Arrays.hashCode(name);
-        result = 31 * result + Arrays.deepHashCode(matrix);
+        int result = getMatrix().hashCode();
+        result = 31 * result + getName().hashCode();
         return result;
+    }
+
+    public List<List<Integer>> getMatrix() {
+        List<List<Integer>> mmm = new ArrayList<>();
+        for (int i = 0; i < this.getSize(); i++) {
+            mmm.add(new ArrayList<>());
+            for (int j = 0; j < this.getSize(); j++) {
+                List<Integer> list = mmm.get(i);
+                list.add(this.matrix.get(i).get(j));
+                mmm.set(i, list);
+            }
+        }
+        return mmm;
+    }
+
+    public List<String> getName() {
+        List<String> str = new ArrayList<>();
+        for (int i = 0; i < this.getSize(); i++) {
+            str.add(this.name.get(i));
+        }
+        return str;
+    }
+
+    public int getSize() {
+        return this.matrix.size();
     }
 
     @Override
     public String toString() {
+        int i, j, k, p;
         StringBuilder sb = new StringBuilder();
-        if (size == 0) {
+        if (this.getSize() == 0) {
             sb.append("Пустой граф" + "\n");
         } else {
-            for (int j = 0; j < size; j++) {
-                for (int i = 0; i < size; i++) {
+            sb.append(" ");
+            for (i = 0; i < this.name.size(); i++) {
+                sb.append(this.name.get(i));
+                   sb.append(" ");
+            }
+            sb.append("\n");
+            for (i = 0; i < this.name.size(); i++) {
+                sb.append(this.name.get(i));
+                for (j = 0; j < this.name.size(); j++) {
+                    if (this.matrix.get(i).get(j) != null) {
+                        sb.append(this.matrix.get(i).get(j));
 
-                    if (i == 0 & j == 0) sb.append("_|");
-                    else if (i == 0) sb.append(name[j - 1] + " ");
-                    else if (j == 0) sb.append(name[i - 1] + " ");
-                    else if (i == j) sb.append(0 + " ");
-                    else if (matrix[i][j] == 0) sb.append("- ");
-                    else sb.append(matrix[i][j] + " ");
+                        p = this.matrix.get(i).get(j);
+                        if (p < 0) p = -p * 10;
+                        k = 0;
+                        while (p > 0) {
+                            p /= 10;
+                            k++;
+                        }
+                        if (k == 0) k = 1;
+                            sb.append(" ");
+                    } else sb.append("- ");
                 }
                 sb.append("\n");
             }
@@ -115,5 +111,106 @@ public class Graph {
         return sb.toString();
     }
 
+    private Graph help() {
+        int i;
+        int size = this.name.size();
+        if (this.matrix.size() > this.name.size()) size = this.matrix.size();
+        for (i = 0; i < this.matrix.size(); i++) {
+            if (this.matrix.get(i).size() > size) size = this.matrix.get(i).size();
+        }
+        for (i = this.name.size(); i < size; i++) {
+            String str2 = java.lang.Integer.toString(i + 1);
+            this.name.add(str2);
+        }
+        for (i = 0; i < this.name.size(); i++)
+            if (this.name.lastIndexOf(this.name.get(i)) != i) throw new IllegalArgumentException("Повторение имени");
+        List<List<Integer>> mat = this.matrix;
+        for (i = 0; i < size; i++)
+            if (i >= this.matrix.size()) mat.add(new ArrayList<>());
+        this.matrix = mat;
+        for (i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                if (j >= this.matrix.get(i).size()) {
+                    List<Integer> list = this.matrix.get(i);
+                    list.add(null);
+                    mat.set(i, list);
+                }
+        this.matrix = mat;
+        return this;
+    }
 
+    public List<Pair<String, Integer>> output(String a) {
+        int i, point;
+        point = this.name.indexOf(a);
+        if (point == -1) throw new IllegalArgumentException("Нет такой вершины");
+        List<Pair<String, Integer>> list = new ArrayList<>();
+        for (i = 0; i < this.getSize(); i++) {
+            if (this.matrix.get(i).get(point) != null) {
+                list.add(new Pair(this.name.get(i), this.matrix.get(i).get(point)));
+            }
+        }
+        return list;
+    }
+
+    public List<Pair<String, Integer>> input(String b) {
+        int i, point;
+        point = this.name.indexOf(b);
+        if (point == -1) throw new IllegalArgumentException("Нет такой вершины");
+        List<Pair<String, Integer>> list = new ArrayList<>();
+        for (i = 0; i < this.getSize(); i++) {
+            if (this.matrix.get(i).get(point) != null) {
+                list.add(new Pair(this.name.get(i), this.matrix.get(point).get(i)));
+            }
+        }
+        return list;
+    }
+
+    public Graph addTrack(String begin, String end, Integer length) {
+        int end1 = this.name.indexOf(end);
+        int begin1 = this.name.indexOf(begin);
+        if ((end1 == -1) || (begin1 == -1))
+            throw new IllegalArgumentException("Не существует вершины");
+        List<Integer> list = this.matrix.get(end1);
+        list.remove(begin1);
+        list.add(begin1, length);
+        this.matrix.set(end1, list);
+        return help();
+    }
+
+    public Graph deleteTrack(String begin, String end) {
+        this.addTrack(begin, end, null);
+        return this;
+    }
+
+    public Graph renameTrack(String begin, String end, Integer length) {
+        return this.addTrack(begin, end, length);
+    }
+
+    public Graph addPoint(String point) {
+        this.name.add(point);
+        return help();
+    }
+
+    public Graph deletePoint(String point) {
+        int i, Point;
+        Point = this.name.indexOf(point);
+        if (Point == -1)
+            throw new IllegalArgumentException("Вершины не существует");
+        for (i = 0; i < this.matrix.size(); i++) {
+            List<Integer> list = this.matrix.get(i);
+            list.remove(Point);
+            this.matrix.set(i, list);
+        }
+        this.matrix.remove(Point);
+        this.name.remove(Point);
+        return this;
+    }
+
+    public Graph renamePoint(String name1, String name2) {
+        int end1 = this.name.indexOf(name1);
+        if (end1 == -1)
+            throw new IllegalArgumentException("Вершины не существует");
+        this.name.set(end1, name2);
+        return help();
+    }
 }
